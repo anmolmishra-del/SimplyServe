63d9fba1a3be84f18a6863cd30dc1b674a8f090b
anmolmishra-del <anmol.mishra@ftprotech.com>
2025-11-11 13:11:03 +0530

    OnBording Page

---
+++ simplyserve/lib/screen/onbording_page/onbording_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OnbordingPage extends StatefulWidget {
+  const OnbordingPage({super.key});
+
+  @override
+  State<OnbordingPage> createState() => _OnbordingPageState();
+}
+
+class _OnbordingPageState extends State<OnbordingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
