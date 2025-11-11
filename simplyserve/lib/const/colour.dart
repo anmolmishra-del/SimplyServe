4411af586bcbee24a9f99061a9c7c95e1dd8f705
anmolmishra-del <anmol.mishra@ftprotech.com>
2025-11-11 13:39:42 +0530

    First commit

--- simplyserve/lib/const/colour.dart
+++ simplyserve/lib/const/colour.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  // 🔹 Primary brand colors
-  static const Color primary = Color(0xFFf76c0f); // Deep purple
 
+  static const Color primary = Color(0xFFf76c0f); 
+
+ static const Color white = Color.fromARGB(255, 254, 254, 254);
 }
